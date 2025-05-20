2572977a14bc6a82776d8ba9f30d0653a1dc1772
krishan <astrohari09@outlook.com>
2025-05-20 20:34:40 +0530

    Update config.py
    Signed-off-by: krishan <astrohari09@outlook.com>

--- config/config.py
+++ config/config.py
@@ -107,10 +107,10 @@ GIT_TOKEN = getenv(
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/TheTeamVivek"
+    "SUPPORT_CHANNEL", "https://t.me/KRISHAN_POLITICSSS"
 )  # Example:- https://t.me/TheTeamVivek
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/TheTeamVk"
+    "SUPPORT_GROUP", "https://t.me/CUTIEE_BOT_SUPPORT"
 )  # Example:- https://t.me/TheTeamVk
 
 
@@ -135,7 +135,7 @@ TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
 
 # Your Github Repo.. Will be shown on /start Command
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/TheTeamVivek/YukkiMusic")
+GITHUB_REPO = getenv("GITHUB_REPO", "https://t.me/KRISHAN_POLITICSSS")
 
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
